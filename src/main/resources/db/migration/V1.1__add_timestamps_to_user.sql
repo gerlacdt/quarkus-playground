2df23bef56e8935322212d029e1b5e4be953d65f
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-05-16 14:37:12 +0200

    add postgres setup with flyway migrations

---
+++ src/main/resources/db/migration/V1.1__add_timestamps_to_user.sql
@@ -0,0 +1,2 @@
+alter table users add column created_at timestamp without time zone;
+alter table users add column updated_at timestamp without time zone;
--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
        id serial PRIMARY KEY,
        -- created_at timestamp without time zone NOT NULL,
        -- updated_at timestamp without time zone NOT NULL,
-       username text
+       username text,
        email text NOT NULL,
        age smallint,
        is_premium boolean NOT NULL
