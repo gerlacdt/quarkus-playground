6b8ed97ced38bb50b26f47d3733c92f47b68fd91
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-05-16 13:32:18 +0200

    added duration and counter metrics for endpoints

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,5 +1,8 @@
 package org.acme;
 
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -9,11 +12,15 @@ import javax.ws.rs.core.Response;
 public class ExampleResource {
 
     @GET
+    @Counted(name = "get_hello", description = "count GET /hello requests")
+    @Timed(name = "get_hello_duration", description = "http request duration")
     public HelloResponse hello() {
         return new HelloResponse("hello");
     }
 
     @POST
+    @Counted(name = "post_hello", description = "count POST /hello requests")
+    @Timed(name = "post_hello_duration", description = "http request duration")
     public Response pushHello(HelloRequest param) {
         return Response.status(Response.Status.CREATED).entity(new HelloResponse((param.getMessage()))).build();
     }
