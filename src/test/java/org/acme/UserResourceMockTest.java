b4c9552ead8202c27cc1d6a68242bb9b4861f9b3
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-05-17 15:57:11 +0200

    added mock test

---
+++ src/test/java/org/acme/UserResourceMockTest.java
@@ -0,0 +1,53 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+public class UserResourceMockTest {
+
+    @InjectMock
+    UserService userService;
+
+    @BeforeEach
+    public void beforeEach() {
+        var user1 = createDefaultUser();
+        var user2 = createDefaultUser();
+        user2.username = "danger";
+        Mockito.when(userService.findAll()).thenReturn(List.of(user1, user2));
+    }
+
+    @Test
+    public void getAll_user_ok() {
+
+        var response = given()
+                .when()
+                .get("/users")
+                .then()
+                .contentType(ContentType.JSON)
+                .extract()
+                .as(GetUsersResponse.class);
+
+        var expected = 2;
+        assertEquals(expected, response.getUsers().size());
+    }
+
+
+    private User createDefaultUser() {
+        var user = new User();
+        user.age = 35;
+        user.email = "john.doe@gmail.com";
+        user.username = "John Doe";
+        user.premium = true;
+        return user;
+    }
+}
