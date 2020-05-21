ed9f7a24f352776f7b89a02747dda5f1f02a107d
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-05-21 14:38:51 +0200

    added tests for jwt token with secured, authorized endpoints

---
+++ src/test/java/org/acme/jwt/TokenSecuredResourceIT.java
@@ -0,0 +1,78 @@
+package org.acme.jwt;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@QuarkusTest
+public class TokenSecuredResourceIT {
+
+  private static final Logger log = LoggerFactory.getLogger(TokenSecuredResourceIT.class);
+
+  @Test
+  public void getToken_valid_ok() {
+
+    var actual =
+        given()
+            .when()
+            .get("/secured/token")
+            .then()
+            .contentType(ContentType.JSON)
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .as(TokenReponse.class);
+
+    // log.info("token={}", actual.getAccessToken());
+    assertNotNull(actual.getAccessToken());
+  }
+
+  @Test
+  public void permitAll_valid_ok() {
+    given()
+        .when()
+        .get("/secured/permit-all")
+        .then()
+        .contentType(ContentType.TEXT)
+        .statusCode(HttpStatus.SC_OK);
+  }
+
+  @Test
+  public void rolesAllowed_noJwt_fail() {
+    given().when().get("/secured/roles-allowed").then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+  }
+
+  @Test
+  public void rolesAllowed_withJwt_ok() {
+    var tokenResponse =
+        given()
+            .when()
+            .get("/secured/token")
+            .then()
+            .contentType(ContentType.JSON)
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .as(TokenReponse.class);
+
+    log.info("token={}", tokenResponse.getAccessToken());
+    var body =
+        given()
+            .header("Authorization", " Bearer " + tokenResponse.getAccessToken())
+            .accept(ContentType.TEXT)
+            .when()
+            .get("/secured/roles-allowed")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .contentType(ContentType.TEXT)
+            .extract()
+            .body()
+            .asString();
+
+    log.info("body={}", body);
+  }
+}
