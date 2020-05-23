190c9c66c8caf5f1eee04366468d160297a83fd1
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-05-23 18:15:08 +0200

    added Postmatching filter, logging matched URI

---
+++ src/main/java/org/acme/middleware/Filter.java
@@ -0,0 +1,21 @@
+package org.acme.middleware;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Provider
+public class Filter implements ContainerRequestFilter {
+
+  Logger log = LoggerFactory.getLogger(Filter.class);
+
+  public void filter(ContainerRequestContext ctx) {
+    var uriInfo = ctx.getUriInfo();
+    var matchedUris = uriInfo.getMatchedURIs();
+    log.info("uris={}", matchedUris);
+  }
+}
